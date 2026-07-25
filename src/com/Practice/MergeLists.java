1745ccecff175018fb10941512b9867967b2a706
shreyakale08 <kaleshreya347@gmail.com>
2026-07-25 23:08:02 +0530

    Merge Two sorted List

---
+++ src/com/Practice/MergeLists.java
@@ -0,0 +1,71 @@
+package com.Practice;
+
+
+public class MergeLists {
+
+public ListNode twoListsMerge(ListNode list1, ListNode list2){
+		
+		ListNode dummy = new ListNode(-1);
+		
+		ListNode current = dummy;
+		
+		while(list1 != null && list2 != null){
+			
+			if(list1.val <= list2.val){
+			
+			current.next = list1;
+				list1 = list1.next;
+		}
+		
+		else{
+			
+			current.next = list2;
+			list2 = list2.next;
+		}		
+ 			current = current.next;
+			
+		}
+	     
+		if(list1 != null){
+			
+			current.next = list1;
+			
+			
+		}	
+		else{
+			
+			current.next = list2;
+		}	
+			
+		
+		return dummy.next;
+	}
+	public static void main(String[] args) {
+		
+		//first sorted Linked List 
+		ListNode list1 = new ListNode(6);
+		
+		list1.next = new ListNode(7);
+		list1.next.next = new ListNode(8);
+		
+		//Second sorted Linked List
+		ListNode list2 = new ListNode(3);
+		
+		list2.next = new ListNode(4);
+		list2.next.next = new ListNode(5);
+		
+		MergeLists obj = new MergeLists();
+		
+		ListNode result = obj.twoListsMerge(list1, list2);
+		
+		while(result != null){
+			
+			System.out.print(result.val + " ");
+			
+			result = result.next;
+		}
+		
+	}
+	
+
+}
