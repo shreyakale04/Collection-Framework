4a720b12aac002b226628d7370af5a8f2a90e76a
shreyakale08 <kaleshreya347@gmail.com>
2026-07-27 23:16:18 +0530

    Reverse List using ListNode

---
+++ src/com/Practice/ReverseList.java
@@ -0,0 +1,50 @@
+package com.Practice;
+
+
+
+public class ReverseList {
+	public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+
+        while(curr != null) {
+
+            // Store next node
+            ListNode next = curr.next;
+
+            // Reverse connection
+            curr.next = prev;
+
+
+            // Move pointers
+            prev = curr;
+            curr = next;
+        }
+
+
+        return prev;
+    }
+
+	public static void main(String[] args) {
+		
+		ListNode head = new ListNode(50);
+		
+		head.next = new ListNode(53);
+		head.next.next = new ListNode(41);
+		head.next.next.next = new ListNode(42);
+		
+		ReverseList obj = new ReverseList();
+		
+	      ListNode result = obj.reverseList(head);
+	      
+	      while(result != null){
+	    	  
+	    	  System.out.print(result.val + " ");
+
+	            result = result.next;
+	      }
+	}
+
+}
